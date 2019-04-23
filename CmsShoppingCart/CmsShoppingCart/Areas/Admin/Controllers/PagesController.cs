71b6a7ace72722e09767143b06b8b7adc838c00f
vixo341 <malpa341@gmail.com>
2019-04-23 23:11:19 +0200

    admin get / post page

--- CmsShoppingCart/CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -24,5 +24,71 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             //return view with list
             return View(pagesList);
         }
+
+
+        //GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+
+        //POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+
+            //check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+
+
+                // Declare slug
+                string slug;
+                // init pageDTO
+                PageDTO dto = new PageDTO();
+
+                // DTO title
+                dto.Title = model.Title;
+                // check for and set slug if need be
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+                // make sure title and slug are unique
+                if (db.Pages.Any(x=> x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "that title or slug already exist.");
+                    return View(model);
+                }
+                // DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+                // save dto
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            //set tempdata message
+            TempData["SM"] = "You have added a new Page!";
+
+
+            //redirect
+            return RedirectToAction("AddPage");
+
+            return View();
+        }
     }
 }
